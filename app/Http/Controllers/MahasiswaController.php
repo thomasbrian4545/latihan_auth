0919ba8893ca376f053d7bda73867e9975d5c6ec
thomasbrian4545 <thomasbrian4545@gmail.com>
2023-09-25 22:05:38 +0700

    Menampilkan data user

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MahasiswaController extends Controller
 {
     public function daftarMahasiswa()
     {
-        return view('halaman', ['judul' => 'Daftar Mahasiswa']);
+        // return view('halaman', ['judul' => 'Daftar Mahasiswa']);
+        echo Auth::user()->id . "<br>";
+        echo Auth::user()->name . "<br>";
+        echo Auth::user()->email . "<br>";
+        echo Auth::user()->password . "<br>";
+
+        dump(Auth::user());
     }
     public function tabelMahasiswa()
     {
